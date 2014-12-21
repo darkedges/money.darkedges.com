b535af9bcf19b89f9ba06971782425f02d349ee5
Nicholas Irving <nirving@darkedges.com>
2014-12-22 10:59:01 +1100

    Nicholas Irving: updated * Fixed the calculators to provide the correct values * Updated the templates

--- app/scripts/directives/howmuch.js
+++ app/scripts/directives/howmuch.js
@@ -14,21 +14,18 @@ angular.module('moneydarkedgescomApp')
                 term: '=',
                 ir: '='
             },
-            template: '<span>{{totalTime}}</span>',
+            template: '<span>{{totalAmount}}</span>',
             restrict: 'AEC',
             replace: true,
             link: function postLink(scope, element, attrs) {
                 scope.$watchGroup(['spm', 'term', 'ir'], function () {
                     var current = 0,
+                        nper = scope.term,
+                        pmt = -1 * scope.spm,
+                        pv = -1 *current,
                         rate = scope.ir / 1200,
-                        pmt = scope.spm,
-                        pv = current,
-                        fv = -1 * scope.goal,
-                        type = 0,
-                        totalMonths = Math.ceil(Math.log((-fv * rate + pmt * (1 + rate * type)) / (pv * rate + pmt * (1 + rate * type))) / Math.log(1 + rate));
-                    var months = $moment(),
-                        tMonths = $moment(months).add(totalMonths, 'months');
-                    scope.totalTime = moment.preciseDiff(months, tMonths);;
+                        type = 0;
+                    scope.totalAmount =  Math.ceil(-(pv * Math.pow(1 + rate, nper) + pmt * ((1 + rate * type) * (Math.pow(1 + rate, nper) - 1) / rate)))
                 });
             }
         };
--- app/scripts/directives/monthly.js
+++ app/scripts/directives/monthly.js
@@ -14,21 +14,18 @@ angular.module('moneydarkedgescomApp')
                 term: '=',
                 ir: '='
             },
-            template: '<span>{{totalTime}}</span>',
+            template: '<span>{{totalPerMonth}}</span>',
             restrict: 'AEC',
             replace: true,
             link: function postLink(scope, element, attrs) {
                 scope.$watchGroup(['goal', 'term', 'ir'], function () {
                     var current = 0,
-                        rate = scope.ir / 1200,
-                        pmt = scope.spm,
-                        pv = current,
+                        nper = scope.term,
+                        pv = -1 * current,
                         fv = -1 * scope.goal,
-                        type = 0,
-                        totalMonths = Math.ceil(Math.log((-fv * rate + pmt * (1 + rate * type)) / (pv * rate + pmt * (1 + rate * type))) / Math.log(1 + rate));
-                    var months = $moment(),
-                        tMonths =  $moment(months).add(totalMonths,'months');
-                    scope.totalTime = moment.preciseDiff(months, tMonths);;
+                        rate = scope.ir / 1200,
+                        type = 0;
+                    scope.totalPerMonth = Math.floor(-((fv + pv * Math.pow(1 + rate, scope.term)) / ((1 + rate * type) * ((Math.pow(1 + rate, scope.term) - 1) / rate))));
                 });
             }
         };
